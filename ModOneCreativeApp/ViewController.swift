8bb1b5dbe14ff4a238c6f1ec80571e67f09f3332
Ava Schmidt <avamschmidt07@gmail.com>
2024-08-28 07:49:43 -0500

    Initial Commit

---
+++ ModOneCreativeApp/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  ModOneCreativeApp
+//
+//  Created by AVA SCHMIDT on 8/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayOutlet: UILabel!
+    @IBOutlet weak var headingOutlet: UILabel!
+    @IBOutlet weak var withdrawOutlet: UITextField!
+    @IBOutlet weak var depositOutlet: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    var deposit = 0.0
+    var withdraw = 0.0
+    var money = 0.0
+    var d = 0.0
+    var w = 0.0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+
+
+    @IBAction func withdrawAction(_ sender: UIButton) {
+        
+      var withdraw = withdrawOutlet.text!
+        if let w = Double(withdraw){
+            money = money - w
+            headingOutlet.text = "Your Balance Is:"
+            displayOutlet.text = "$\(money)"
+        }
+        else{
+            print("Error")
+            displayOutlet.text = "Error: Invalid Entry"
+        }
+        withdrawOutlet.resignFirstResponder()
+        
+       
+        imageView.image = UIImage(named: "money")
+    }
+    @IBAction func depositAction(_ sender: UIButton) {
+        
+        var deposit = depositOutlet.text!
+        if let d = Double(deposit){
+            money = d + money
+            headingOutlet.text = "Your Balance Is:"
+            displayOutlet.text = "$\(money)"
+        }
+        else{
+            print("Error")
+            displayOutlet.text = "Error: Invalid Entry"
+        }
+        depositOutlet.resignFirstResponder()
+        
+        
+        
+        imageView.image = UIImage(named: "money")
+    }
+    
+}
+
